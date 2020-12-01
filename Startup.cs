cbb615ce371bf00f39d38bb29f51cb4ba9d90905
Patrick Sudol <patrick.sudol@outlook.com>
2020-11-30 22:04:20 -0500

    Rearranged middleware

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,7 @@ namespace NespressoReviewsApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseMySql(Configuration.GetConnectionString("DefaultConnection"),
@@ -64,8 +65,6 @@ namespace NespressoReviewsApi
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
